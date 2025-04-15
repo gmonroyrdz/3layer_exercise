5003cd9ef293ef5ef7c865a0a83db60faf72ad5b
gmonroyr <gmonroyr@gmail.com>
2025-04-15 16:59:41 -0600

    Versión completa Swagger, OpenApi y API Restful con Jersey

--- src/main/java/mx/uam/Main.java
+++ src/main/java/mx/uam/Main.java
@@ -8,8 +8,9 @@ import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
 
+import java.io.File;
 import java.net.URI;
 
 public class Main {
@@ -21,16 +22,24 @@ public class Main {
         final ResourceConfig config = new ResourceConfig()
                 .packages("mx.uam.resource") // Paquete donde están tus recursos
                 .register(OpenApiResource.class); // Registro de Swagger OpenAPI
-                //.register(JacksonJaxbJsonProvider.class); // Registro de Jackson para JSON
+  
 
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+        // Servir archivos estáticos de Swagger UI en /docs
+        //StaticHttpHandler staticHandler = new StaticHttpHandler("src/main/resources/swagger-ui");
+        StaticHttpHandler staticHandler = new StaticHttpHandler(new File("src/main/resources/swagger-ui/").getAbsolutePath());
+        staticHandler.setFileCacheEnabled(false); // (opcional para desarrollo)
+        
+        
+        server.getServerConfiguration().addHttpHandler(staticHandler, "/docs");
+        return server;
     }
 
     public static void main(String[] args) {
         final HttpServer server = startServer();
         System.out.println("Servidor corriendo en " + BASE_URI + "clients");
         System.out.println("Swagger JSON disponible en: " + BASE_URI + "openapi.json");
-        System.out.println("Swagger UI disponible en: " + BASE_URI + "swagger-ui/");
+        System.out.println("Swagger UI disponible en: " + "http://localhost:8080/docs/index.html");
         try {
             Thread.currentThread().join(); // Mantener el servidor vivo
         } catch (InterruptedException e) {
---
+++ target/classes/swagger-ui/swagger-initializer.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  window.ui = SwaggerUIBundle({
+    url: "/api/openapi.json",
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout"
+  });
+};
+
